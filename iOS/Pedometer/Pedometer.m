af45a3a234a8d0d43f434f91cfe517ac6700a7e9
Yusuf Olokoba <olokobayusuf@gmail.com>
2018-09-17 21:34:09 -0400

    Added native iOS and Android source projects

---
+++ iOS/Pedometer/Pedometer.m
@@ -0,0 +1,13 @@
+//
+//  Pedometer.m
+//  Pedometer
+//
+//  Created by Yusuf Olokoba on 9/17/18.
+//  Copyright © 2018 Yusuf Olokoba. All rights reserved.
+//
+
+#import "Pedometer.h"
+
+@implementation Pedometer
+
+@end
